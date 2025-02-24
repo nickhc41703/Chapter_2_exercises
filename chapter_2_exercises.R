4c7c9ba33c271d6505312fd18f4bffd0793c3489
nickhc41703 <nicholascamacho21@augustana.edu>
2025-02-24 12:57:02 -0600

    Add files via upload

--- chapter_2_exercises.R
+++ chapter_2_exercises.R
@@ -1,5 +1,5 @@
 library("ggplot2")
-#exersice 1
+#exercise 1
 x3 <- c(0, 1, 1, 2, 2, 2, 3, 3, 4)
 qplot(x3, binwidth = 1)
 #exercise 2
@@ -10,5 +10,24 @@ roll <- function() {
   sum(dice)
 }
 #exercise 2 test
+rolls <- replicate(10000, roll())
+qplot(rolls, binwidth = 1)
+
+#book examples
+x <- c(-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1)
+x
+y <- x^3
+y
+qplot(x, y)
+
+x <- c(1, 2, 2, 2, 3, 3)
+qplot(x, binwidth = 1)
+
+x2 <- c(1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4)
+qplot(x2, binwidth = 1)
+
+replicate(3, 1 + 1)
+replicate(10, roll())
+
 rolls <- replicate(10000, roll())
 qplot(rolls, binwidth = 1)
